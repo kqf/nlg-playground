2c36384c5808a900a58ce5815ee6e6e41629086e
Sasha <q.dev.null@gmail.com>
2019-12-22 09:27:36 +0100

    Remove the global variables

--- model/dataset.py
+++ model/dataset.py
@@ -2,30 +2,30 @@ import json
 import pandas as pd
 from environs import Env
 
-env = Env()
-env.read_env()
-TARGET_NAME = env("TARGET_NAME")
 
-
-def target_idx(data):
+def target_idx(data, target_name):
     contants = [x["name"] for x in data["frequent_contacts"]["list"]]
-    return contants.index(TARGET_NAME)
+    return contants.index(target_name)
 
 
 def main():
+    env = Env()
+    env.read_env()
+    target_name = env("TARGET_NAME")
+
     with open("data/result.json") as f:
         data = json.load(f)
 
-    idx = target_idx(data)
+    idx = target_idx(data, target_name)
     df = pd.DataFrame(data["chats"]["list"][idx]["messages"])
-    df = df[df["from"] == TARGET_NAME].reset_index()
+    df = df[df["from"] == target_name].reset_index()
     df["text"] = df["text"].str.replace('\n', ' ')
     df = df[df["text"].astype(bool)]
     df = df[~df["text"].str.contains(r"\[{'").astype(bool)]
     df["text"] = df["text"].str.strip()
     print(df["text"].head(5))
-    df["text"].to_csv("data/input.txt", header=False, index=False, sep="@")
     # from IPython import embed; embed()
+    df["text"].to_csv("data/input.txt", header=False, index=False, sep="@")
 
 
 if __name__ == '__main__':
