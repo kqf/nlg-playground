c49af44f7208b916b6f4663709d27734ae05507b
Sasha <q.dev.null@gmail.com>
2023-05-24 08:50:44 +0100

    Fix new vars

--- tests/test_dummy.py
+++ tests/test_dummy.py
@@ -11,6 +11,8 @@ from unittest.mock import patch
         "MESSAGE_THANKS": "Thanks",
         "MESSAGE_START": "Hey",
         "MESSAGE_HELP": "Help",
+        "MODEL": "123",
+        "MESSAGE_VERSION": "123",
     },
 )
 def test_imports():
