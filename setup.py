6284a30b009f9d54f3f276e6e0873580ae8c5341
Sasha <q.dev.null@gmail.com>
2023-07-20 12:51:34 +0200

    Fix the setup

--- setup.py
+++ setup.py
@@ -1,17 +1,5 @@
-from setuptools import find_packages, setup
+from setuptools import setup
 
-setup(
-    name="nlg-playground",
-    version="0.0.1",
-    packages=find_packages(),
-    entry_points={
-        "console_scripts": [
-            "nlg-train=model.main:train",
-            "nlg-generate=model.main:generate",
-            "nlg-dialogue=model.main:dialogue",
-            "nlg-dataset=model.dataset:main",
-            "nlg-raw-data=model.dataset:download",
-            "oleg=bot.run:main",
-        ],
-    },
-)
+# Editable installs:
+# https://setuptools.pypa.io/en/latest/userguide/pyproject_config.html
+setup()
